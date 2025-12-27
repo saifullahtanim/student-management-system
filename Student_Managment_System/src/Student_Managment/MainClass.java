5414f2382734f82ebb581a09778d5442ba0655e8
Saifulla Tanim <okmrtanim69@gmail.com>
2025-12-28 01:28:05 +0600

    MainClass.java

--- Student_Managment_System/src/Student_Managment/MainClass.java
+++ Student_Managment_System/src/Student_Managment/MainClass.java
@@ -1,14 +1,10 @@
 package Student_Managment;
 
-import javax.swing.JFrame;
-
-public class MainClass extends JFrame {
+public class MainClass {
 
     public static void main(String[] args) {
-        Login window = new Login();
-        window.setVisible(true);
-        window.setBounds(100, 50, 420, 350);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setTitle("Login");
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
     }
 }
